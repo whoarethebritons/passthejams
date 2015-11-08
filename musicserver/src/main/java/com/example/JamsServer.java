7e7b137d007654ecb3cc2e64ceab645aed0af4e9
David Martel <davidkmartel@gmail.com>
2015-11-08 10:25:48 -0800

    Server can successfully copy songs. The NetworkTest activity will copy all songs from the server.

--- musicserver/src/main/java/com/example/JamsServer.java
+++ musicserver/src/main/java/com/example/JamsServer.java
@@ -13,10 +13,13 @@ import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
 import org.jaudiotagger.tag.TagException;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -26,19 +29,85 @@ import java.util.HashMap;
 public class JamsServer implements Runnable {
     private static ServerSocket serverSocky;
     private static int nextSongId = 1;
-    private static HashMap<JsonObject,Integer> idLookpu = new HashMap<>();
-    private static HashMap<Integer,String> pathLookup = new HashMap<>();
+    private static int nextAlbumId = 1;
+    private static HashMap<JsonObject,Integer> idLookup = new HashMap<>();
+    private static HashMap<Integer,Song> songLookup = new HashMap<>();
+    private static HashMap<String,Integer> albumIdLookup = new HashMap<>();
     private Socket socky;
+    public static final int BUFFER_SIZE = 1024;
+
+    private static JsonArray songs = null;
 
     public static void main(String args[]) throws IOException {
         serverSocky = new ServerSocket(8080);
         //System.out.println(new JamsServer(null).getSongs());
+        loadSongs();
         while(true) {
             Socket s = serverSocky.accept();
             JamsServer server = new JamsServer(s);
             (new Thread(server)).run();
         }
 
+    }
+
+    private static int getSongId(String title, String artist, String album) {
+        JsonObject song = new JsonObject();
+        if(title == null) return 0;
+        if(artist == null) artist = "";
+        if(album == null) album = "";
+        song.addProperty("title",title);
+        song.addProperty("artist",artist);
+        song.addProperty("album", album);
+        return getSongId(song);
+
+    }
+
+    private static int getSongId(JsonObject key) {
+        //ensure that there aren't any extra properties
+        JsonObject tempKey = new JsonObject();
+        tempKey.add("title", key.get("title"));
+        tempKey.add("artist", key.get("artist"));
+        tempKey.add("album", key.get("album"));
+        int id = 0;
+        if(idLookup.containsKey(tempKey)) {
+            id = idLookup.get(tempKey);
+        } else {
+            id = nextSongId++;
+            idLookup.put(tempKey,id);
+        }
+        return id;
+    }
+
+    private static int getAlbumId(String album) {
+        //TODO: will fail at some point, multiple albums can have the same name
+        if(albumIdLookup.containsKey(album)) {
+            return  albumIdLookup.get(album);
+        } else {
+            int id = nextAlbumId++;
+            albumIdLookup.put(album,id);
+            return id;
+        }
+    }
+
+    private static void loadSongs() {
+        songs = loadSongs(new File("."));
+    }
+
+    private static JsonArray loadSongs(File dir) {
+        JsonArray songs = new JsonArray();
+        for(File f:dir.listFiles()) {
+            if(f.isDirectory()) {
+                songs.addAll(loadSongs(f));
+            } else {
+                String name = f.getName();
+                if(name.matches("(.*mp3$)|(.*m4p$)|(.*m4a$)|(.*ogg$)")) {
+                    Song song = new Song(f);
+                    songLookup.put(song._id,song);
+                    songs.add(song.toJson());
+                }
+            }
+        }
+        return songs;
     }
 
     public JamsServer(Socket s) {
@@ -57,10 +126,14 @@ public class JamsServer implements Runnable {
                 return;
             }
             line = line.trim();
+            System.out.println("Command: "+line);
             if(line.equals("getSongs")) {
                 out.print(getSongs());
+                out.flush();
             } else if(line.contains("getSong")) {
-                out.print("Unimplemented");
+                int songId = Integer.parseInt(line.substring(8));
+                Song song = songLookup.get(songId);
+                copyFile(song.file);
             } else if(line.equals("getAlbums")) {
                 out.print("Unimplemented");
             } else if(line.equals("getPlaylists")) {
@@ -78,48 +151,84 @@ public class JamsServer implements Runnable {
                 e.printStackTrace();
             }
         }
+        System.out.println("Done");
     }
 
     private String getSongs() {
-        return new Gson().toJson(getSongs(new File(".")));
+        return new Gson().toJson(songs);
     }
 
-    private JsonArray getSongs(File dir) {
-        JsonArray songs = new JsonArray();
-        for(File f:dir.listFiles()) {
-            if(f.isDirectory()) {
-                songs.addAll(getSongs(f));
-            } else {
-                String name = f.getName();
-                if(name.matches("(.*mp3$)|(.*m4p$)|(.*m4a$)|(.*ogg$)")) {
-                    JsonObject song = new JsonObject();
-                    //song.addProperty("FileName",name);
-                    try {
-                        AudioFile ff = AudioFileIO.read(f);
-                        Tag tag = ff.getTag();
-                        //[_id, title, artist, album, album_id]
-                        String jamsId = tag.getFirst("JamsId");
-                        song.addProperty("_id","0");
-                        song.addProperty("title",tag.getFirst(FieldKey.TITLE));
-                        song.addProperty("artist",tag.getFirst(FieldKey.ARTIST));
-                        song.addProperty("album",tag.getFirst(FieldKey.ALBUM));
-                        song.addProperty("album_id","0");
-                    } catch (TagException e) {
-                        e.printStackTrace();
-                    } catch (ReadOnlyFileException e) {
-                        e.printStackTrace();
-                    } catch (CannotReadException e) {
-                        e.printStackTrace();
-                    } catch (InvalidAudioFrameException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println(song);
-                    songs.add(song);
-                }
+    private void copyFile(File f) throws IOException {
+        System.out.println("Serving file: " + f.getName());
+        byte buffer[] = new byte[BUFFER_SIZE];
+        try(FileInputStream in = new FileInputStream(f)) {
+            OutputStream out = socky.getOutputStream();
+            int read = 0;
+            while ((read = in.read(buffer)) > 0) {
+                out.write(buffer, 0, read);
             }
         }
-        return songs;
+        System.out.println("Served file: " + f.getName());
+    }
+
+    private static class Song {
+        public String title = null;
+        public String artist = null;
+        public String album = null;
+        public int _id = 0;
+        public int album_id = 0;
+        public File file = null;
+
+        public Song() {
+
+        }
+
+        public Song(String title, String artist, String album, File file) {
+            this.title = title;
+            this.artist = artist;
+            this.album = album;
+            this._id = getSongId(title, artist,album);
+            this.album_id = getAlbumId(album);
+            this.file = file;
+        }
+
+        public Song(File file) {
+            AudioFile ff = null;
+            try {
+                ff = AudioFileIO.read(file);
+                Tag tag = ff.getTag();
+                title = tag.getFirst(FieldKey.TITLE);
+                artist = tag.getFirst(FieldKey.ARTIST);
+                album = tag.getFirst(FieldKey.ALBUM);
+                _id = getSongId(title, artist, album);
+                album_id = getAlbumId(album);
+                this.file = file;
+            } catch (CannotReadException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (TagException e) {
+                e.printStackTrace();
+            } catch (ReadOnlyFileException e) {
+                e.printStackTrace();
+            } catch (InvalidAudioFrameException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public JsonObject toJson() {
+            JsonObject obj = new JsonObject();
+            obj.addProperty("title",title);
+            obj.addProperty("artist",artist);
+            obj.addProperty("album",album);
+            obj.addProperty("_id",_id);
+            obj.addProperty("album_id",album_id);
+            obj.addProperty("file_name",file.getName());
+            return obj;
+        }
+
+        public String toString() {
+            return new Gson().toJson(toJson());
+        }
     }
 }
