de51307a42289cb46136f05707ae6e6a289ce4d2
whoarethebritons <eden.akins@gmail.com>
2015-11-12 12:49:28 -0800

    changed art back to non async

--- app/src/main/java/com/passthejams/app/JamsCursorAdapter.java
+++ app/src/main/java/com/passthejams/app/JamsCursorAdapter.java
@@ -2,6 +2,7 @@ package com.passthejams.app;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,17 +10,15 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.SimpleCursorAdapter;
 
+import java.util.concurrent.ExecutionException;
+
 /**
  * Created by Eden on 11/8/2015.
  */
 public class JamsCursorAdapter extends SimpleCursorAdapter {
     final String TAG = "JamsCursorAdapter";
-    class ImageViewHolder {
-        ImageView imageView;
-    }
     Context mContext;
     int mLayout;
-    ImageViewHolder mHolder;
 
 
     public JamsCursorAdapter(Context context, int layout, Cursor c, String[] from, int[] to) {
@@ -29,7 +28,6 @@ public class JamsCursorAdapter extends SimpleCursorAdapter {
     }
     @Override
     public void setViewImage(ImageView v, String value) {
-        Log.v(TAG, value);
-        new Shared.ImageLoader().execute(v, value, mContext);
+        Shared.getAlbumArt(mContext, v, value);
     }
 }
