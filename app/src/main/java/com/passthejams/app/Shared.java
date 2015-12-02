5a75f74633c030c8be61e79207dbf35c807bdcd7
David Martel <DavidKMartel@gmail.com>
2015-12-02 15:46:31 -0800

    Cached loading of album art, making it faster.

--- app/src/main/java/com/passthejams/app/Shared.java
+++ app/src/main/java/com/passthejams/app/Shared.java
@@ -8,6 +8,9 @@ import android.util.Log;
 import android.widget.ImageView;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
 
 /**
  * Created by Eden on 7/21/2015.
@@ -49,17 +52,35 @@ public class Shared {
             MediaStore.Audio.Albums.ARTIST
     };
 
+    private static HashMap<Integer,Uri> albumArtLookup = new HashMap<>();
+
     static void getAlbumArt(Context mContext, ImageView v, String album_id) {
         int id = Integer.parseInt(album_id);
-        Uri test = ContentUris.withAppendedId(
-                Uri.parse("content://media/external/audio/albumart"), id);
-        try {
-            mContext.getContentResolver().openInputStream(test);
-            v.setImageURI(test);
-        }
-        catch(FileNotFoundException e) {
-            Log.e("Shared", e.getMessage());
-            v.setImageResource(R.drawable.default_album);
+        if(albumArtLookup.containsKey(id)) {
+            Uri uri = albumArtLookup.get(id);
+            if(uri != null) {
+                v.setImageURI(uri);
+            } else {
+                v.setImageResource(R.drawable.default_album);
+            }
+        } else {
+            Uri test = ContentUris.withAppendedId(
+                    Uri.parse("content://media/external/audio/albumart"), id);
+            InputStream in = null;
+            try {
+                in = mContext.getContentResolver().openInputStream(test);
+                albumArtLookup.put(id, test);
+                v.setImageURI(test);
+            } catch (FileNotFoundException e) {
+                Log.e("Shared", e.getMessage());
+                albumArtLookup.put(id, null);
+                v.setImageResource(R.drawable.default_album);
+            } finally {
+                if (in != null) try {
+                    in.close();
+                } catch (IOException e) {
+                }
+            }
         }
     }
 
